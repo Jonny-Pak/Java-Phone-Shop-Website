b94bda16ff5dd6a872893308ad9ec517c7943149
Ngọc Thuận <thuanln1907@gmail.com>
2025-11-08 23:23:17 +0700

    setup spring framework

---
+++ src/test/java/website/ban/ien/thoai/PhoneSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package website.ban.ien.thoai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
